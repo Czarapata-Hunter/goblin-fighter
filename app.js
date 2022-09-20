d5accea49d24504f7f9da2cbf025cf97ca7e4b57
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-20 11:55:27 -0700

    Adding the DOM Elements that will be needed

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 /* Imports */
 
 /* Get DOM Elements */
+const predator = document.getElementById('predator');
+const playerHealth = document.getElementById('player-health');
+
+const enemyInput = document.getElementById('enemy-input');
+const enemyList = document.getElementById('enemy-list');
+const removeEnemy = document.getElementById('remove-enemy');
+
+const playerBoard = document.getElementById('player-board');
+const damageBoard = document.getElementById('damage-board');
 
 /* State */
 
