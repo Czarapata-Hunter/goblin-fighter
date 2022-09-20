c23285e95f44d041a793f63b775f7458c458a6ca
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-20 12:31:13 -0700

    Display of User (Health, Alive/Dead)

--- app.js
+++ app.js
@@ -12,9 +12,20 @@ const playerBoard = document.getElementById('player-board');
 const damageBoard = document.getElementById('damage-board');
 
 /* State */
-
+let player = {
+    health: 100,
+    type: 'champion',
+};
 /* Events */
 
 /* Display Functions */
-
+function displayPlayer() {
+    playerHealth.textContent = Math.max(0, player.health);
+    if (player.health < 1) {
+        predator.src = 'assets/predator_dead.png';
+    } else {
+        predator.src = `assets/${player.type}.png`;
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
+displayPlayer();
